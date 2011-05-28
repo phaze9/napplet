16dafbbe6de09521515d79098e5f96b4637d7650
phaze9 <phaze9@carmenita.local>
2011-05-29 00:45:10 +0200

    Version bumped to 0.4.0 because incompatible with pre 1.5 Processing versions.

--- src/napplet/NApplet.java
+++ src/napplet/NApplet.java
@@ -31,7 +31,7 @@ public class NApplet extends PApplet implements Nit, MouseWheelListener,
 	/**
 	 * Library version.
 	 */
-	public static final String VERSION = "0.3.1";
+	public static final String VERSION = "0.4.0";
 
 	/**
 	 * Returns library version.
